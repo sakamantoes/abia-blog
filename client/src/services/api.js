a5333bfd8cfb3ecdde5334bad5d8c95a8dcb6e04
leoTechHub <godswilltrade@gmail.com>
2026-04-13 23:51:29 +0100

    testing

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const Base_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
+const Base_URL = import.meta.env.VITE_API_BASE_URL || 'https://lisa-unmodified-noble.ngrok-free.dev/api';
 
 const api = axios.create({
   baseURL: Base_URL,
